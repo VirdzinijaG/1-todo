69a24037ca709a20c5af9879e25589fef0e31496
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-07-12 11:54:48 +0300

    Form input validation

--- js/components/form/Form.js
+++ js/components/form/Form.js
@@ -3,6 +3,11 @@ class Form {
         this.selector = selector;
 
         this.DOM = null;
+        this.messageDOM = null;
+        this.colorDOM = null;
+        this.dateDOM = null;
+        this.completedDOM = null;
+        this.saveButtonDOM = null;
     }
     init() {
         // patikrtinti, ar validus selector
@@ -12,6 +17,7 @@ class Form {
         }
         this.render();
         this.findTargetELement();
+        this.addEvents();
         // susirandame DOM elementa, kuriame reikia ipiesti forma
         console.log("tesiam darba...");
     }
@@ -38,7 +44,7 @@ class Form {
                         <textarea placeholder="Message.."></textarea>
                     </div>
                     <div class="form-row">
-                        <input type="color">
+                        <input type="color" value="#3366ff">
                     </div>
                     <div class="form-row">
                         <input type="date">
@@ -55,6 +61,63 @@ class Form {
         // this.DOM.innerHTML = HTML;
         // this.DOM.innerHTML += HTML;
         this.DOM.insertAdjacentHTML("afterbegin", HTML);
+
+        this.messageDOM = this.saveButtonDOM = this.DOM.querySelector("textarea");
+        this.colorDOM = this.saveButtonDOM = this.DOM.querySelector('input[type="color"]');
+        this.dateDOM = this.saveButtonDOM = this.DOM.querySelector('input[type="date"]');
+        this.completedDOM = this.saveButtonDOM = this.DOM.querySelector('input[type="checkbox"]');
+        this.saveButtonDOM = this.saveButtonDOM = this.DOM.querySelector("button.save");
+
+
+
+
+        // this.saveButtonDOM = document.querySelector("");
+        this.saveButtonDOM = this.DOM.querySelector("button.save");
+        // this.saveButtonDOM = this.DOM.querySelector(".save");
+        // this.saveButtonDOM = this.DOM.querySelector('button[type="submit"]');
+        
+    }
+    addEvents() {
+        this.saveButtonDOM.addEventListener('click', (event) => {
+            event.preventDefault();
+            // console.log('click click...');
+            const message = this.messageDOM.value;
+            const color = this.colorDOM.value;
+            const date = this.dateDOM.value;
+            const completed = this.completedDOM.checked;
+
+           if (this.isValidTask(message, color, date, completed)) {
+               console.log("task is valid...");
+           } else {
+               console.log("task is NOT valid...");
+           }
+
+            console.log(message);
+            console.log(color);
+            console.log(date);
+            console.log(completed);
+        });
+    }
+    isValidTask() {
+        if (!this.isValidMessage(message) ||
+            !this.isValidColor(color) ||
+            !this.isValidDate(date) ||
+            !this.isValidCompleted(completed)) {
+            return false;
+        }
+        return true;
+    }
+    isValidMessage(message) {
+        return true;
+    }
+    isValidColor(color) {
+        return true;
+    }
+    isValidDate(date) {
+        return true;
+    }
+    isValidCompleted(completed) {
+        return true;
     }
 }
 

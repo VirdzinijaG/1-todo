5a2cb4e04eeb88b26873eb5f083b06cff3978fdf
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-07-13 11:14:19 +0300

    Todo createCard method

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 "use strict";
 
 import { Form as Forma } from "./components/form/Form.js";
+import { Todo } from "./components/todo/Todo.js";
 // importavimas failo 
 
 const form = new Forma("body");
 form.init();
 
 
-console.log(form);
+// console.log(form);
+
+const tasks = new Todo('body');
+tasks.init();
+
+tasks.createCard('Kortele veikia', '#36f', '2030-12-31', 'true');
